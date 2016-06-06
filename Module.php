a441e22be68ab1c5e79bca1d9df085d238d277b4
Madone <ahamad.jedu@gmail.com>
2016-06-06 18:06:13 +0700

    New module

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace thaisouthdev\article;
+
+/**
+ * article module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'thaisouthdev\article\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
